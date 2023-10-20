01e69cf1e039379c7598e6f9e9114c65db8c72cd
B242415-2023 <hi_Al>
2023-10-20 09:06:52 +0100

    created replicate index

--- comparisons.sh
+++ comparisons.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+#1. Mean the replicates for each condition
+
+touch ./temp/replicateindex.txt
+awk '{FS="\t";}{if(NR!=1){print $2,$4,$5}}' ./seqdata/Tco2.fqfiles | 
+sort - | 
+uniq - > ./temp/replicateindex.txt
+
+replicatewc=$(wc -l ./temp/replicateindex.txt| awk 'BEGIN{FS=" ";}{print $1}')
+for number in {1..${replicatewc}};
+do
+clone=$(awk 'BEGIN{FS="\t";}{if(NR == $number){print $1}}' ./seqdata/Tco2.fqfiles)
+time=$(awk 'BEGIN{FS="\t";}{if(NR == $number){print $2}}' ./seqdata/Tco2.fqfiles)
+state=$(awk 'BEGIN{FS="\t";}{if(NR == $number){print $3}}' ./seqdata/Tco2.fqfiles)
+
+echo ${clone},${time},${state}
+done
+
+for number in {1..}
+
+> ./temp/replicateindex.txt
+
+
+
 #1. USER DEFINED GROUP SELECTIONS
 
 
@@ -13,13 +36,3 @@
 #search for same clone and treatment, then take range for all replicates 
 
 
-while read 
-do
-
-while read gene 
-do
-grep -n gene 
-
-done < ./refseqdata/TriTrypDB-46_TcongolenseIL3000_2019.bed
-
-done < 
